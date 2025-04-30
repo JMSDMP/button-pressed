859c5a9051595e4b2748f7952f78186a8ad4848b
James Dempsey <jamesdempsey07@gmail.com>
2025-04-30 13:03:34 +0100

    allow for inputs on touch devices

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-window.addEventListener("keypress", (ev) => {
-    showButton(ev.key)
-})
+let inputEle = document.createElement("input")
+
+if ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) {
+    document.addEventListener("DOMContentLoaded", () => {
+        inputEle.addEventListener("input", (ev) => {
+            showButton(ev.target.value.slice(-1));
+            ev.target.value = "";
+        });
+        document.body.appendChild(inputEle);
+        inputEle.focus();
+    });
+} else {
+    window.addEventListener("keypress", (ev) => {
+        showButton(ev.key);
+    });
+}
+
 
 function showButton(key) {
     let newButtonEle = document.createElement("div");
@@ -9,14 +23,5 @@ function showButton(key) {
     document.body.appendChild(newButtonEle)
     setTimeout(()=>{document.body.removeChild(newButtonEle)},1000)
 }
-// Add an input field to trigger the keyboard on mobile devices
-const inputField = document.createElement("input");
-inputField.type = "text";
-inputField.style.position = "absolute";
-inputField.style.opacity = "0"; // Make it invisible
-inputField.style.height = "0";
-inputField.style.width = "0";
-document.body.appendChild(inputField);
 
-// Focus the input field to open the keyboard
-inputField.focus();
+
